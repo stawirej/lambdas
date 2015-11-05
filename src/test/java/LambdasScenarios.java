07c9106fc392ad131663181696531a5ec3378f82
Piotr Stawirej <piotr.stawirej@gmail.com>
2015-11-05 14:58:12 +0100

    Streams.

--- src/test/java/LambdasScenarios.java
+++ src/test/java/LambdasScenarios.java
@@ -1,10 +1,13 @@
 import static org.fest.assertions.api.Assertions.assertThat;
 
 import java.awt.event.ActionListener;
+import java.util.List;
 import java.util.function.BinaryOperator;
 import java.util.function.LongBinaryOperator;
 import java.util.stream.IntStream;
+import java.util.stream.Stream;
 
+import org.fest.util.Lists;
 import org.junit.Test;
 
 public class LambdasScenarios {
@@ -29,6 +32,24 @@ public class LambdasScenarios {
         System.out.println("i = " + i);
         final long j = addLong.applyAsLong(2L, 3L);
         System.out.println("j = " + j);
+
+    }
+
+    @Test
+    public void streams() {
+        final List<Integer> table = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        // int evenCounter = 0;
+        // for (int i = 0; i < table.size(); i++) {
+        // if (table.get(i) % 2 == 0) {
+        // evenCounter++;
+        // }
+        // }
+
+        final long evenCounter = table.stream().filter(x -> x % 2 == 0).count();
+        System.out.println("evenCounter = " + evenCounter);
+
+        final Object[] mapped = table.stream().map(x -> x * 10).toArray();
+        System.out.println("mapped = " + mapped.);
     }
 
     @Test
