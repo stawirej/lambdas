f1bb85682d9c7ae413b5fc5d4792326e93e1cb7f
Piotr Stawirej <piotr.stawirej@gmail.com>
2015-11-19 16:09:00 +0100

    Method reference. Ordering. Custom collectors.

---
+++ src/main/java/Person.java
@@ -0,0 +1,12 @@
+public class Person {
+
+    private final String name;
+
+    public Person(final String name) {
+        this.name = name;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
--- src/test/java/LambdasScenarios.java
+++ src/test/java/LambdasScenarios.java
@@ -1,8 +1,10 @@
 import static org.fest.assertions.api.Assertions.assertThat;
 
 import java.awt.event.ActionListener;
+import java.util.Collection;
 import java.util.List;
 import java.util.Set;
+import java.util.TreeSet;
 import java.util.function.BinaryOperator;
 import java.util.function.LongBinaryOperator;
 import java.util.stream.Collectors;
@@ -13,6 +15,10 @@ import org.fest.util.Lists;
 import org.junit.Test;
 
 public class LambdasScenarios {
+
+    private static boolean isEven(final int number) {
+        return number % 2 == 0;
+    }
 
     @Test
     public void shouldShowLambdasExpressionsForms() {
@@ -152,10 +158,66 @@ public class LambdasScenarios {
         // Given
         final Set<Integer> collected =
             IntStream.of(1, 2, 3, 4, 5, 6, 7, 8, 9, 10).filter(x -> x > 5).boxed().collect(Collectors.toSet());
+    }
+
+    @Test
+    public void shouldUseMethodReference() {
+        // Given
+        final List<Integer> table = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+        final List<Person> people = Lists.newArrayList(new Person("Brandon"), new Person("Jessica"), new Person("Marry"));
+
+        // When
+        final List<String> names = people.stream().map(Person::getName).collect(Collectors.toList());
+        System.out.println("names = " + names);
+
+        final List<Integer> collect = table.stream().filter(LambdasScenarios::isEven).collect(Collectors.toList());
+        System.out.println("collect = " + collect);
+
+        table.stream().reduce(0, (acc, element) -> acc += element).intValue();
+        final int sum = table.stream().reduce(0, Integer::sum).intValue();
+        System.out.println("sum = " + sum);
+    }
+
+    @Test
+    public void shouldOrder() {
+        // Given
+        final List<Integer> table = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
+
+        // When
+        final Set<Integer> set = table.stream().collect(Collectors.toSet());
+        // Replace first and last digit in table
+        System.out.println("set = " + set);
+
+        final Set<Integer> sortedSet = table.stream().sorted().collect(Collectors.toSet());
+        System.out.println("sortedSet = " + sortedSet);
+    }
+
+    @Test
+    public void shouldPickCustomCollector() {
+        // Given
+        final List<Integer> table = Lists.newArrayList(1, 2, 3, 4, 5, 6, 7, 8, 9, 10);
 
         // When
 
-        // Then
-    }
+        // STEP ONE
+        //
+        // final Supplier<Collection<Integer>> collectionFactory = new Supplier<Collection<Integer>>() {
+        //
+        // @Override
+        // public Collection<Integer> get() {
+        // return new TreeSet<>();
+        // }
+        // };
+        // final Collection<Integer> set = table.stream().collect(Collectors.toCollection(collectionFactory));
 
+        // STEP TWO
+        // final Collection<Integer> set = table.stream().collect(Collectors.toCollection(() -> new TreeSet<Integer>()));
+
+        // STEP THREE
+        final Collection<Integer> set = table.stream().collect(Collectors.toCollection(TreeSet::new));
+
+        // Then
+        System.out.println("set = " + set);
+        System.out.println("supplier = " + set.getClass());
+    }
 }
